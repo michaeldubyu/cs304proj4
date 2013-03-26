8262c37875dc629a3f3b336803a054cd26f81c91
David Fernig <david.fernig@gmail.com>
2013-03-25 21:06:57 -0700

    finally this shit is done

--- src/Reports.java
+++ src/Reports.java
@@ -6,7 +6,6 @@ public class Reports {
 	private static Connection con;
 	private static final long CURRENT_TIME = (System.currentTimeMillis() / 1000L);
 	
-	
 	/*
 	 * Generate a report with the most popular items in a given year. The librarian provides 
 	 * a year and a number n. The system lists out the top n books that where borrowed the 
@@ -98,37 +97,40 @@ public class Reports {
 		
 		try 
 		{
-			
+			/*
 			if (subject.equals(""))
 			{
+				System.out.println("empty string");
 				stmt = con.createStatement();
 				//rs = stmt.executeQuery("SELECT * FROM borrowing ORDER BY callNumber ASC");
 				
-				rs = stmt.executeQuery("SELECT o.callnumber, b1.name, b2.copyNumber, o.outDate, o.inDate "+
-										"FROM borrowing o, book b " +
-										"WHERE o.callNumber = b1.callNumber and b1.callNumber = b2.callNumber " +
+				rs = stmt.executeQuery("SELECT o.callnumber, b1.title, b2.copyNo, o.outDate, o.inDate "+
+										"FROM borrowing o, book b1, bookCopy b2  " +
+										"WHERE o.callNumber = b1.callNumber AND b1.callNumber = b2.callNumber " +
 										"ORDER BY o.callNumber ASC");
 				
 			}
 			
 			else
 			{
+				System.out.println("non-empty string");
+				*/
 				stmt = con.createStatement();
 				//rs = stmt.executeQuery("SELECT * FROM (SELECT * FROM borrowing o, hasSubject s WHERE "+
 				//                       "s.subject = "+subject+" AND o.callNumber = s.callNumber)" + 
 				//		               "ORDER BY callNumber ASC");
-				rs = stmt.executeQuery("SELECT o.callnumber, b1.name, b2.copyNumber, o.outDate, o.inDate "+
+				rs = stmt.executeQuery("SELECT o.callnumber, b1.title, b2.copyNo, o.outDate, o.inDate "+
 										"FROM borrowing o, book b1, bookCopy b2, hasSubject s " +
-										"WHERE o.callNumber = b1.callNumber AND b1.callNumber = b2.callNumber AND s.subject = '"+subject+"' "+
+										"WHERE o.callNumber = b1.callNumber AND b1.callNumber = b2.callNumber AND s.subject LIKE '%"+subject+"%' "+
 										"ORDER BY o.callNumber ASC");
 				
-			}
+			//}
 			
 			while(rs.next())
 			{
 				ArrayList<String> aBorrowing = new ArrayList<String>();
 				String overdue = "";
-				int inDate = Integer.parseInt(rs.getString(6));
+				int inDate = Integer.parseInt(rs.getString(5));
 					
 				if (CURRENT_TIME > inDate)
 					overdue = "Item Overdue";
