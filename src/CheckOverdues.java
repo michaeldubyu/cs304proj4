f5cfc287e7907f74986cd18bd91eddc4f15a1647
Jonathan Stiansen <jonathan.stiansen@gmail.com>
2013-03-30 12:39:23 -0700

    Got rid of submit button on check overdues. Go straight to check. Incomplete - task : 304-Project 3

--- src/CheckOverdues.java
+++ src/CheckOverdues.java
@@ -15,87 +15,62 @@ public class CheckOverdues {
 	public CheckOverdues(Frame frame){
 
 		final Frame searchFrame = new Frame("Books on Loan");
-		//final JTextField title = new JTextField(20);
-		//final JTextField author = new JTextField(20);
 		final JTextField subject = new JTextField(20);
 
-		searchFrame.setLayout(new GridLayout(	6,3));
-
-		//Label titleLabel = new Label("Call Number :");
-		//Label authorLabel = new Label("Out Date :");
-		//Label subjectLabel = new Label("Subject :");
-		//Label overdueLabel = new Label("Overdue? :");
-
-		//searchFrame.add(titleLabel);
-		//searchFrame.add(title);
-		//searchFrame.add(authorLabel);
-		//searchFrame.add(author);
-		//searchFrame.add(subjectLabel);
-		//searchFrame.add(subject);
+		searchFrame.setLayout(new GridLayout(	2,1));
 
 		Button submit = new Button("Submit");
-		//searchFrame.add(new Label("(*)Required fields marked.")); //to pad the submit button to the right
 		searchFrame.add(submit);
 		searchFrame.setLocationRelativeTo(frame);
-		searchFrame.pack();
-		searchFrame.setVisible(true);
-		//title.requestFocus();	
+		//when the submit button is clicked
+		try{
+			ArrayList<ArrayList<String>> result = BorrowingTable.displayOverdueItems();
 
-		submit.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				//when the submit button is clicked
-				try{
-					ArrayList<ArrayList<String>> result = BorrowingTable.displayOverdueItems();
-
-					final Frame successFrame = new Frame("Results");
-					if(result.size() > 0){
-						//callnumber, name, count
-						Object rowData[][] = new Object[result.size()][3];
-						int i = 0;
-						for (ArrayList<String> s:result){
-							rowData[i] = s.toArray();
-							i++;
-						}
-
-						Object columnNames[] = { "Borrower ID", "Borrower Name", "Title", "Out Date", "email", "Borrower Type" };
-						JTable table = new JTable(rowData, columnNames);
-
-						JScrollPane scroll = new JScrollPane(table);
-
-						successFrame.add(scroll);
-						successFrame.pack();
-						successFrame.setVisible(true);
-						successFrame.setAlwaysOnTop(true);
-						successFrame.setLocationRelativeTo(searchFrame);
-						successFrame.addWindowListener( new WindowAdapter() {
-							public void windowClosing(WindowEvent we) {
-								successFrame.setVisible(false);
-								searchFrame.dispose();
-
-							}
-						} );
-					}
-					else {
-						JLabel noResults = new JLabel("There are no fines outstanding");
-						successFrame.add(noResults);
-					}
-				}catch(Exception argException){
-					final Frame errorFrame = new Frame("Error!");
-					Label error = new Label(argException.getMessage());
-					errorFrame.add(error);
-					errorFrame.pack();
-					errorFrame.setVisible(true);
-					errorFrame.setAlwaysOnTop(true);
-					errorFrame.setLocationRelativeTo(searchFrame);
-					errorFrame.addWindowListener( new WindowAdapter() {
-						public void windowClosing(WindowEvent we) {
-
-							errorFrame.setVisible(false);
-						}
-					} );
+			final Frame successFrame = new Frame("Results");
+			if(result.size() > 0){
+				//callnumber, name, count
+				Object rowData[][] = new Object[result.size()][3];
+				int i = 0;
+				for (ArrayList<String> s:result){
+					rowData[i] = s.toArray();
+					i++;
 				}
+				Object columnNames[] = { "Borrower ID", "Borrower Name", "Title", "Out Date", "email", "Borrower Type" };
+				JTable table = new JTable(rowData, columnNames);
+				JScrollPane scroll = new JScrollPane(table);
+				successFrame.add(scroll);
 			}
-		});
+			else {
+				JLabel noResults = new JLabel("There are no fines outstanding");
+				successFrame.add(noResults);
+			}
+			successFrame.pack();
+			successFrame.setVisible(true);
+			successFrame.setAlwaysOnTop(true);
+			successFrame.setLocationRelativeTo(searchFrame);
+			successFrame.addWindowListener( new WindowAdapter() {
+				public void windowClosing(WindowEvent we) {
+					successFrame.setVisible(false);
+					searchFrame.dispose();
+
+				}
+			} );
+		}catch(Exception argException){
+			final Frame errorFrame = new Frame("Error!");
+			Label error = new Label(argException.getMessage());
+			errorFrame.add(error);
+			errorFrame.pack();
+			errorFrame.setVisible(true);
+			errorFrame.setAlwaysOnTop(true);
+			errorFrame.setLocationRelativeTo(searchFrame);
+			errorFrame.addWindowListener( new WindowAdapter() {
+				public void windowClosing(WindowEvent we) {
+
+					errorFrame.setVisible(false);
+				}
+			} );
+		}
+
 
 		searchFrame.addWindowListener( new WindowAdapter() {
 			public void windowClosing(WindowEvent we) {
